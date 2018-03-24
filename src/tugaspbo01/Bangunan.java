85237ca310b68e8a15eb4c31eb73946e2f50b9ca
Muhammad Rahmani <muhammadrahmani84@gmail.com>
2018-03-24 21:37:10 +0800

    Menambahkan Objeck baru beserta nilai dan perbaikan

--- src/tugaspbo01/Bangunan.java
+++ src/tugaspbo01/Bangunan.java
@@ -10,17 +10,19 @@ package tugaspbo01;
  * @author Asus
  */
 public class Bangunan {
+    String nama;
     String lebar;
     String tinggi;
     String bahan;
     String lokasi;
-    String properti;
+    String item;
     
     void cetakInfo(){
+        System.out.println("Objeck   :"+nama);
         System.out.println("Lebar    :"+lebar);
         System.out.println("tinggi   :"+tinggi);
         System.out.println("Bahan    :"+bahan);
         System.out.println("Lokasi   :"+lokasi);
-        System.out.println("Properti :"+properti);
+        System.out.println("Item :"+item);
     }
 }
--- src/tugaspbo01/TugasPBO01Aksi.java
+++ src/tugaspbo01/TugasPBO01Aksi.java
@@ -8,12 +8,35 @@ public class TugasPBO01Aksi {
     public static void main(String[] args) {
         Bangunan Rumah = new Bangunan();
         
+        Rumah.nama="Rumah";
         Rumah.lebar="14 Meter";
         Rumah.tinggi="7 Meter";
         Rumah.bahan="Kayu, Semen";
         Rumah.lokasi="Desa";
-        Rumah.properti="Tempat Tidur";
+        Rumah.item="Kasur, TV";
         
         Rumah.cetakInfo();
+        
+        Bangunan Hotel = new Bangunan();
+        
+        Hotel.nama="Hotel";
+        Hotel.lebar="23 Meter";
+        Hotel.tinggi="35 Meter";
+        Hotel.bahan="Semen";
+        Hotel.lokasi="Kota";
+        Hotel.item="Kasur, TV";
+                
+        Hotel.cetakInfo();
+        
+        Bangunan Kantor = new Bangunan();
+        
+        Kantor.nama="Kantor";
+        Kantor.lebar="18 Meter";
+        Kantor.tinggi="7 Meter";
+        Kantor.bahan="Kayu, Semen";
+        Kantor.lokasi="Desa/Kota";
+        Kantor.item="Meja, Kursi, Panan Tulis";
+        
+        Kantor.cetakInfo();
     }
 }
