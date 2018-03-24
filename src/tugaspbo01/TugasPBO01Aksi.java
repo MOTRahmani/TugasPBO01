6c05396de60fd731dba3357ef62c9437c01fe056
Muhammad Rahmani <muhammadrahmani84@gmail.com>
2018-03-24 21:19:54 +0800

    Mengisi Nilai atribut dan perbaikan

--- src/tugaspbo01/TugasPBO01Aksi.java
+++ src/tugaspbo01/TugasPBO01Aksi.java
@@ -7,5 +7,12 @@ package tugaspbo01;
 public class TugasPBO01Aksi {
     public static void main(String[] args) {
         Bangunan Rumah = new Bangunan();
+        
+        Rumah.lebar="14 Meter";
+        Rumah.tinggi="7 Meter";
+        Rumah.bahan="Kayu, Semen";
+        Rumah.lokasi="Desa";
+        Rumah.properti="Tempat Tidur";
+          
     }
 }
